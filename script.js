7a2cb80bf17db04620aa156245444a99670cbf36
Laura Bigoni <laurabigoni.92@gmail.com>
2022-05-31 19:50:51 +0200

    completed exercise

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // prendo gli elementi dal DOM
 const submit = document.getElementById('submit');
-let newImage = document.querySelector('input[type=url]');
+let newImage = document.querySelector('input[type="url"]');
 
-let image = document.getElementById('image-preview');
-const filterControls = document.querySelectorAll('input[type=range]');
+let image = document.querySelector('img');
+let labels = document.querySelectorAll('label:not([for="image"])');
+const filterControls = document.querySelectorAll('input[type="range"]');
 
 // Creo una funzione per la validazione dell'url
 function checkURL(url) {
-  return (url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+  return (url.match(/\.(jpeg|jpg|gif|png|bmp|webp)$/) != null);
 }
 
 // Creo la funzione per caricare un immagine da un link
@@ -18,6 +19,7 @@ function changeImage() {
     alert('The image URL you provided is not valid. Please try again with another URL.');
   }
   newImage.value = '';
+  image.setAttribute('style', '');
 };
 
 // Creo la funzione per i filtri
@@ -27,9 +29,14 @@ function applyFilter() {
     // Qui devo dare lo style all'immagine
     filters += item.getAttribute('data-filter') + '(' + item.value + item.getAttribute('data-scale') + ') ';
   });
-  image.setAttribute("style", "filter:" + filters);
-  // image.style.filter = filters;
-  console.log(filters);
-  console.log(image);
+  image.setAttribute('style', 'filter: ' + filters + ';');
+  // console.log(filters);
+  // console.log(image);
 };
 
+// Faccio comparire l'input range al click del label
+for (let i = 0; i < labels.length; i++) {
+  labels[i].addEventListener('click', function () {
+    filterControls[i].classList.toggle("d-none");
+  })
+}
